1d6741d4ba68b0bb6efd569fa5d82f53fa23c48e
Chicho <chichoxian@gmail.com>
2019-12-19 17:21:32 +0800

    Add files via upload

--- detectTPL1.py
+++ detectTPL1.py
@@ -7,7 +7,7 @@ from random import shuffle
 
 appPath = "/home/zhanxian/workspace/LibDetection/LibID/profiles/app/"
 
-libPath = "/home/zhanxian/workspace/LibDetection/LibID/profiles/Lib1/"
+libPath = "/home/zhanxian/workspace/LibDetection/LibID/profiles/Lib2/"
 
 outputPath = "./outputs/"
 
@@ -15,12 +15,13 @@ appList = os.listdir(appPath)
 
 libList = os.listdir(libPath)
 
+shuffle(appList)
 
 for app in appList:
 
     eachappPath = os.path.join(appPath,app)
 
-    eachappOutPath = os.path.join("outputs/Lib1",app)
+    eachappOutPath = os.path.join("outputs/Lib2",app)
 
     print eachappOutPath
 
@@ -28,7 +29,7 @@ for app in appList:
         print "this " + app + " has existed"
         continue 
 
-    cmd = "./LibID.py detect -af {0} -ld {1} -o {2}".format(eachappPath,libPath,"outputs/Lib1/")
+    cmd = "./LibID.py detect -af {0} -ld {1} -o {2}".format(eachappPath,libPath,"outputs/Lib2/")
     os.system(cmd)
 
 
